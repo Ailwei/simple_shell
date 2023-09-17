55190d6bf56d2e7f6b1cea8971ad15fb9503efff
Ailwei <Ailweimaemu@gmail.com>
2023-09-17 11:25:54 -0700

    updated main.c

--- main.c
+++ main.c
@@ -1,109 +1,94 @@
 #include "shell.h"
 
+/**
+ * main - Simple Shell main function
+ *
+ * Description: This function implements a simple shell.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
 int main(void)
 {
-	 char *shell_name = "hsh";
-char *line;	
+	 char *line;
 	 size_t command_length;
-	char command[MAX_COMMAND_LENGTH];
-	/*get path env */
+    char *shell_name = "hsh";
+    char command[MAX_COMMAND_LENGTH];
 
-	char *path_env = getenv("PATH");
+    /* Get the PATH environment variable */
+    char *path_env = getenv("PATH");
     if (path_env == NULL)
     {
         perror("Unable to get PATH");
         return 1;
     }
 
-	while (1)
-	{
-		/*Display the prompt */
-		printf("%s> ", shell_name);
-		fflush(stdout);
+    while (1)
+    {
+        /* Display the shell prompt */
+        printf("%s> ", shell_name);
+        fflush(stdout);
 
-		/*read the command line using custom_getline */
-		line = custom_getline();
+        /* Read a command line using custom_getline */
+        line = custom_getline();
 
+        /* Check for Ctrl + D (EOF) */
+        if (line == NULL)
+        {
+            printf("\nExiting %s.\n", shell_name);
+            break;
+        }
 
+        /* Copy the line to the command buffer */
+        strncpy(command, line, sizeof(command));
 
-		/*read Read a command line */
+        /* Remove the trailing newline character */
+        command_length = strlen(command);
+        if (command_length > 0 && command[command_length - 1] == '\n')
+        {
+            command[command_length - 1] = '\0';
+        }
 
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
-			/* Handl Ctrl + D */
-			printf("\nExiting %s.\n", shell_name);
-			break;
-		}
-		/*Remove the trailing newline character */
-		command_length = strlen(command);
-		if (command_length > 0 && command[command_length-1] == '\n')
-		{
-			command[command_length -1] ='\0';
-		}
-		/*Execute the command */
+        /* Free the memory allocated by custom_getline */
+        free(line);
 
-		if (command_length > 0)
-		{
-			if (strcmp(command, "exit") == 0)
-			{
-				/* handle the "exit" builtin command */
-				exit_shell(shell_name);
-				
-			}
-			else if (strncmp(command, "exit", 5) == 0)
-			{
-				/*handle exit with status*/
-				const char *status = command + 5;
-				handle_exit(status);
-			}
-			else
-			{
-				exit_shell(shell_name);
-			}
+        /* Rest of the code to handle built-in commands and execute external commands */
+        if (command_length > 0)
+        {
+            if (strcmp(command, "exit") == 0)
+            {
+                /* Handle the "exit" builtin command */
+                exit_shell(shell_name);
+            }
+            else if (strncmp(command, "exit ", 5) == 0)
+            {
+                /* Handle "exit" with status */
+                const char *status = command + 5;
+                handle_exit(status);
+            }
+            else if (strcmp(command, "env") == 0)
+            {
+                /* Handle the "env" builtin command */
+                env_builtin(environ);
+            }
+            else
+            {
+                /* Execute the command */
+                if (strstr(command, " ") != NULL)
+                {
+                    /* Command has arguments, use execute_command_with_args */
+                    char *space_position = strchr(command, ' ');
+                    const char *arguments = space_position + 1;
+                    execute_cmd_args(command, arguments);
+                }
+                else
+                {
+                    /* Command has no arguments, use execute_command */
+                    execute_command(command);
+                }
+            }
+        }
+    }
 
-			}
-			else if (strcmp(command, "env") == 0)
-			{
-				/*handle the env builtin cmd*/
-				env_builtin(environ);
-			}
-			else
-			{
-				
-			if (strstr(command, " ") != NULL)
-			{
-				/*command has path ariguments use execute_command_arguments*/
-				
-				                       execute_command_with_path(command, path_env, shell_name);
-				execute_command_with_args(command);
-			}
-			else
-			{
-				/*command has  arguments */
-				execute_command_with_args(command);
-			} 
-			if(command_length > 0)
-			{
-				char *space_position = strchr(command, ' ');
-				const char *arguments = space_position + 1;
-				execute_cmd_args(command,arguments);
-			}
-			
-			/*handle no arguments*/
-			execute_command(command);
-
-			}
-			/*handle_cmd_with args */
-
-			/*exit shell*/
-		exit_shell(shell_name);
-		}
-		free(line);
-		
-	}
-}
-}
+    return 0;
 }
 
-return (0);
-}
